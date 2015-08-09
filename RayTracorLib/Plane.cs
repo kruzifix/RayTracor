5dc04d1e00f83421f0ad00bad59b8c5fd674335c
kruzifix <david.cukro@gmail.com>
2015-08-09 23:42:57 +0200

    plane

---
+++ RayTracorLib/Plane.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracor.RayTracorLib
+{
+    public class Plane : Object
+    {
+        Vector normal;
+
+        public Plane(Vector position, Vector normal, Material material)
+            :base(position, material)
+        {
+            this.normal = normal.Normalized;
+        }
+
+        public override IntersectionResult Intersects(Ray ray)
+        {
+            double denom = Vector.DotProduct(normal, ray.Direction);
+            if (Math.Abs(denom) > 0.0001)
+            {
+                //float t = (center - ray.origin).dot(normal) / denom;
+                //if (t >= 0) return true; // you might want to allow an epsilon here too
+                double t = Vector.DotProduct(Position - ray.Start, normal) / denom;
+                if (t > 0.0005)
+                    return new IntersectionResult(true, t);
+            }
+            return new IntersectionResult(false, 0.0);
+        }
+
+        public override Vector Normal(Vector point)
+        {
+            return normal;
+        }
+
+        public override Vector EvalMaterial(Vector point, double lambertAmount)
+        {
+            double x = Math.Floor(point.X);
+            double y = Math.Floor(point.Z);
+
+            Vector col = Material.Color.ToVector();
+            if (x % 2 == 0 ^ y % 2 == 0)
+                col = Color.Black.ToVector();
+            return col * lambertAmount * Material.Lambert + col * Material.Ambient;
+        }
+    }
+}
