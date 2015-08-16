ea0398a8b0b035d9e717a353b29ec2e416dcd815
kruzifix <david.cukro@gmail.com>
2015-08-16 22:39:32 +0200

    changed formshowrender interpolation mode

--- FormShowRender.Designer.cs
+++ FormShowRender.Designer.cs
@@ -28,35 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pBox = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pBox)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pBox
-            // 
-            this.pBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pBox.Location = new System.Drawing.Point(0, 0);
-            this.pBox.Name = "pBox";
-            this.pBox.Size = new System.Drawing.Size(640, 480);
-            this.pBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pBox.TabIndex = 0;
-            this.pBox.TabStop = false;
             // 
             // FormShowRender
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(640, 480);
-            this.Controls.Add(this.pBox);
+            this.ClientSize = new System.Drawing.Size(463, 338);
             this.Name = "FormShowRender";
             this.Text = "FormShowRender";
-            ((System.ComponentModel.ISupportInitialize)(this.pBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pBox;
     }
 }
--- FormShowRender.cs
+++ FormShowRender.cs
@@ -13,27 +13,27 @@ namespace RayTracor
     public partial class FormShowRender : Form
     {
         // offset: x = 16; y = 39
-        Bitmap bmp;
-
         public FormShowRender(Bitmap bmp)
         {
             InitializeComponent();
+            MinimumSize = new Size(bmp.Width+16, bmp.Height+39);
 
-            this.bmp = bmp;
-            this.Width = bmp.Width + 16;
-            this.Height = bmp.Height + 39;
-            this.CenterToScreen();
-            pBox.Paint += pBox_Paint;
+            InterpolPictureBox pBox = new InterpolPictureBox();
+            pBox.Image = bmp;
+            pBox.Dock = DockStyle.Fill;
+            pBox.SizeMode = PictureBoxSizeMode.Zoom;
+
+            Controls.Add(pBox);
         }
+    }
 
-        private void pBox_Paint(object sender, PaintEventArgs e)
+    public class InterpolPictureBox : PictureBox
+    {
+        protected override void OnPaint(PaintEventArgs pe)
         {
-            double height = pBox.Width * bmp.Height * 1.0 / bmp.Width;
-            double width = pBox.Height * bmp.Width * 1.0 / bmp.Height;
-            if (pBox.Width * 1.0 / pBox.Height < bmp.Width * 1.0 / bmp.Height)
-                e.Graphics.DrawImage(bmp, 0, 0, pBox.Width, (int)height);
-            else
-                e.Graphics.DrawImage(bmp, 0, 0, (int)width, pBox.Height);
+            pe.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+
+            base.OnPaint(pe);
         }
     }
 }
