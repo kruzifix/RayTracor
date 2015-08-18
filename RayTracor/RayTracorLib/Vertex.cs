905842bb7809fb4f10f81276caa41570798feb3e
David <kruzifix@users.noreply.github.com>
2015-08-18 17:16:32 +0200

    added Vertex

---
+++ RayTracor/RayTracorLib/Vertex.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RayTracor.RayTracorLib
+{
+    public class Vertex
+    {
+        public Vector Position { get; set; }
+        public Vector2 TexCoord { get; set; }
+        public Vector Color { get; set; }
+    }
+}
