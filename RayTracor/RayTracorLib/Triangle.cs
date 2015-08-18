a65b3e52c7441be43939f1760d95ccfb1f63e1dc
David <kruzifix@users.noreply.github.com>
2015-08-18 17:17:45 +0200

    updated Triangle.cs

--- RayTracor/RayTracorLib/Triangle.cs
+++ RayTracor/RayTracorLib/Triangle.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,29 +9,29 @@ namespace RayTracor.RayTracorLib
 {
     public class Triangle : Object
     {
-        public Vector Vertex0 { get; set; }
-        public Vector Vertex1 { get; set; }
-        public Vector Vertex2 { get; set; }
+        public Vertex Vertex0 { get; set; }
+        public Vertex Vertex1 { get; set; }
+        public Vertex Vertex2 { get; set; }
 
         public Vector E1 { get; private set; }
         public Vector E2 { get; private set; }
         public Vector Normal { get; private set; }
 
-        public Triangle(Vector v0, Vector v1, Vector v2, Material material)
+        public Triangle(Vertex v0, Vertex v1, Vertex v2, Material material)
             :base(Vector.Zero, material)
         {
             Vertex0 = v0;
             Vertex1 = v1;
             Vertex2 = v2;
 
-            E1 = Vertex1 - Vertex0;
-            E2 = Vertex2 - Vertex0;
+            E1 = Vertex1.Position - Vertex0.Position;
+            E2 = Vertex2.Position - Vertex0.Position;
             Normal = Vector.CrossProduct(E2, E1).Normalized;
         }
         
         public override Intersection Intersects(Ray ray)
         {
-            Vector T = ray.Start - Vertex0;
+            Vector T = ray.Start - Vertex0.Position;
             Vector P = Vector.CrossProduct(ray.Direction, E2);
             Vector Q = Vector.CrossProduct(T, E1);
 
@@ -41,8 +41,9 @@ namespace RayTracor.RayTracorLib
                 double u = Vector.DotProduct(P, T) / denom;
                 double v = Vector.DotProduct(Q, ray.Direction) / denom;
                 double t = Vector.DotProduct(Q, E2) / denom;
+                bool kl = denom < 0.0;
                 if ((u >= 0.0 && u <= 1.0) && (v >= 0.0 && u + v <= 1.0))
-                    return new Intersection(t, ray.PointAt(t), this, denom < 0 ? Normal : Normal.Negated, new Vector2(u, v));
+                    return new Intersection(t, ray.PointAt(t), this, kl ? Normal : Normal.Negated, new Vector2(u, kl?v:1-v)); // automatic texture flipping
             }
             return Intersection.False;
         }
@@ -50,17 +51,24 @@ namespace RayTracor.RayTracorLib
         public override Vector EvalMaterial(Intersection intersec, double lambertAmount)
         {
             Vector col = Material.Color.ToVector();
-            //if (bary.X > 0.25 && bary.Y < 0.75 && bary.Y > 0.25 && bary.Y < 0.75)
-            //    col *= 0.5;
+
             if (Material.Textured)
             {
-                double scale = 255.0;
-                int u = (int)Math.Floor(intersec.BaryCoords.X * scale);
-                int v = (int)Math.Floor(intersec.BaryCoords.Y * scale);
+                //double scale = 255.0;
+                //int u = (int)Math.Floor(intersec.BaryCoords.X * scale);
+                //int v = (int)Math.Floor(intersec.BaryCoords.Y * scale);
 
-                double fac = (u ^ v) * 0.75 / scale + 0.25;
-                col *= fac;
+                //double fac = (u ^ v) * 0.75 / scale + 0.25;
+                //col *= fac;
+
+                Vector2 bary = intersec.BaryCoords;
+                double v0contrib = 1 - bary.X - bary.Y;
+                double v1contrib = 1 - bary.Y;
+                double v2contrib = 1 - bary.X;
+
+                col = Material.GetTexCol(Vertex0.TexCoord + (Vertex1.TexCoord - Vertex0.TexCoord) * v1contrib + (Vertex2.TexCoord - Vertex0.TexCoord) * v2contrib);
             }
+
             return Material.AddAmbientLambert(col, lambertAmount);
         }
 
@@ -90,9 +98,9 @@ namespace RayTracor.RayTracorLib
         {
             XmlNode node = doc.CreateElement("triangle");
             Material.Serialize(doc, node);
-            node.AppendChild(Vertex0.Serialize(doc, "vertex0"));
-            node.AppendChild(Vertex1.Serialize(doc, "vertex1"));
-            node.AppendChild(Vertex2.Serialize(doc, "vertex2"));
+            //node.AppendChild(Vertex0.Serialize(doc, "vertex0"));
+            //node.AppendChild(Vertex1.Serialize(doc, "vertex1"));
+            //node.AppendChild(Vertex2.Serialize(doc, "vertex2"));
             parent.AppendChild(node);
         }
 
@@ -103,7 +111,10 @@ namespace RayTracor.RayTracorLib
             Vector v2 = Vector.Parse(node["vertex2"]);
             Material mat = Material.Parse(node["material"]);
 
-            return new Triangle(v0, v1, v2, mat);
+            Triangle t = new Triangle(new Vertex { Position = v0, TexCoord = Vector2.Zero },
+                              new Vertex { Position = v1, TexCoord = Vector2.UnitX },
+                              new Vertex { Position = v2, TexCoord = Vector2.One }, mat);
+            return t;
         }
     }
 }
